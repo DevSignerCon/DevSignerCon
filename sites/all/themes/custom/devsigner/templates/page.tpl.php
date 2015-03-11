e32dd60171484846d3c9a8a19a721e5e2069a7ea
Candice Newton <candice.c.newton@gmail.com>
2015-03-11 10:58:10 -0700

    Forms styling, sidebar layout, page layout

--- sites/all/themes/custom/devsigner/templates/page.tpl.php
+++ sites/all/themes/custom/devsigner/templates/page.tpl.php
@@ -83,43 +83,46 @@
 
     <div id="content-wrapper">
       <div id="content" class="column" role="main">
-        <?php print render($page['highlighted']); ?>
-        <?php print $breadcrumb; ?>
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?>
-          <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+        <div class="content-inner">
+          <?php print render($page['highlighted']); ?>
+          <?php print $breadcrumb; ?>
+          <a id="main-content"></a>
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?>
+            <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php print $messages; ?>
+          <?php print render($tabs); ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+          <?php print render($page['content']); ?>
+          <?php print $feed_icons; ?>
+        </div>
+
+        <?php
+          // Render the sidebars to see if there's anything in them.
+          $sidebar_first  = render($page['sidebar_first']);
+          $sidebar_second = render($page['sidebar_second']);
+        ?>
+
+        <?php if ($sidebar_first || $sidebar_second): ?>
+          <aside class="sidebars">
+            <?php print $sidebar_first; ?>
+            <?php print $sidebar_second; ?>
+          </aside>
         <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php print $messages; ?>
-        <?php print render($tabs); ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
-      </div>
-    </div>
+
+      </div><!-- #content -->
+    </div><!-- #content-wrapper -->
 
     <?php print render($page['tier_1']); ?>
     <?php print render($page['tier_2']); ?>
     <?php print render($page['tier_3']); ?>
     <?php print render($page['tier_4']); ?>
     <?php print render($page['tier_5']); ?>
-
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
-
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside>
-    <?php endif; ?>
 
   </div>
 

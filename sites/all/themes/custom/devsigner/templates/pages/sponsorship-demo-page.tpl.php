0f1d70c3e5534af68916b73c7792bd7e41b78293
Brian Gonzales <brian@pairodime.com>
2016-07-12 15:32:05 -0700

    CSS: New Sponsorship Styles + Demo Page Styles

---
+++ sites/all/themes/custom/devsigner/templates/pages/sponsorship-demo-page.tpl.php
@@ -0,0 +1,288 @@
+<?php
+$sp_name = $_GET["name"];
+$sp_name_logo = str_replace(" ", "+", $sp_name);
+
+$sp_website = $_GET["website"];
+?>
+<section class="sponsor-group platinum">
+
+    <div class="sponsor-label">
+        <h3>Platinum Sponsor</h3>
+    </div>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=<?php print $sp_name_logo; ?>+Co-Sponsor+Photo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div>
+        </header>
+        <div class="sponsor-text">
+            <p>This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event. This would be a great spot for information about how we are co-sponsoring the event.</p>
+        </div>
+    </article>
+</section>
+
+<section class="sponsor-group gold">
+
+    <div class="sponsor-label">
+        <h3>Gold Sponsors</h3>
+    </div>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=<?php print $sp_name_logo; ?>+X-Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div>
+        </header>
+        <div class="sponsor-text">
+            <p>Crafting compelling open source engagement solutions in partnership with forward-thinking clients. Our main office is loted in Portland, and we are actively seeking new talent for various positions in-house so please make sure to stop by the Devsigner Mixer and come chat with us!</p>
+        </div>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=<?php print $sp_name_logo; ?>+X-Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div>
+        </header>
+        <div class="sponsor-text">
+            <p>Crafting compelling open source engagement solutions in partnership with forward-thinking clients. Our main office is loted in Portland, and we are actively seeking new talent for various positions in-house so please make sure to stop by the Devsigner Mixer and come chat with us!</p>
+        </div>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=<?php print $sp_name_logo; ?>+X-Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div>
+        </header>
+        <div class="sponsor-text">
+            <p>Crafting compelling open source engagement solutions in partnership with forward-thinking clients. Our main office is loted in Portland, and we are actively seeking new talent for various positions in-house so please make sure to stop by the Devsigner Mixer and come chat with us!</p>
+        </div>
+    </article>
+
+</section>
+
+<section class="sponsor-group silver">
+
+    <div class="sponsor-label">
+        <h3>Silver Sponsors</h3>
+    </div>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Large+Logo">
+            <h2><?php print $sp_name; ?></h2>
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+</section>
+
+<section class="sponsor-group bronze">
+
+    <div class="sponsor-label">
+        <h3>Bronze Sponsors</h3>
+    </div>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Medium+Logo">
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Medium+Logo">
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Medium+Logo">
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Medium+Logo">
+            <div class="social-info">
+                <ul>
+                    <li><a href="http://www.<?php print $sp_website; ?>" target="_blank"><?php print $sp_website; ?></a></li>
+                </ul>
+            </div>
+        </header>
+    </article>
+
+</section>
+
+<section class="sponsor-group community">
+
+    <div class="sponsor-label">
+        <h3>Community Sponsors</h3>
+    </div>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+    <article class="sponsor">
+        <header>
+            <img class="logo" src="http://placehold.it/900x340?text=Small+Logo">
+        </header>
+    </article>
+
+</section>
